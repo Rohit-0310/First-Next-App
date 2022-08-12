c6fdce3255ea09a0489a48eb39d4d7c9e30dee0d
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-08-12 13:23:13 +0530

    update scss

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-import styles from './style.module.css'
+import styles from './style.module.scss'
 import profile from "../public/Image/profile.JPG"
 import Link from 'next/link'
 import Image from 'next/image'
