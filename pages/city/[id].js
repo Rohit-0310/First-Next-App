0b2b8a37b49d08979b8291117f0fd469b9c7d402
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-08-12 13:12:47 +0530

    update

--- pages/city/[id].js
+++ pages/city/[id].js
@@ -1,4 +1,7 @@
 import { useRouter } from 'next/router'
+import Navbar from '../../components/Navbar';
+import styles from '../../styles/Car.module.scss';
+
 
 const City = () => {
     const router = useRouter();
@@ -6,7 +9,11 @@ const City = () => {
 
   return (
     <div>
+      <Navbar />
+      <div className={styles.main}>
         <h1>Welcome to {id}</h1>
+
+      </div>
     </div>
   )
 }
